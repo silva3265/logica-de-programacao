a58e587f989d53fccfbf415c6e30c585d0487297
silva3265 <silva3265@gmail.com>
2024-01-25 14:48:10 -0300

    Exercício 01: Cadastrando as tarefas do dia

--- src/br/com/modulo05/Vetores1Dimensao.java
+++ src/br/com/modulo05/Vetores1Dimensao.java
@@ -3,9 +3,9 @@ package br.com.modulo05;
 import java.util.Scanner;
 
 public class Vetores1Dimensao {
-	
+
 	public static void main(String[] args) {
-		
+
 //		 String [] cardapio = new String [] {"Calabresa", "Atum", "Queijo", "Presunto"};
 //		 
 //		 System.out.println("Escolha o Sabor: ");
@@ -20,26 +20,24 @@ public class Vetores1Dimensao {
 //		 Integer saborEscolhido = sc.nextInt();
 //		 
 //		 System.out.println("Voce escolheu o sabor: " + cardapio[saborEscolhido]);
-		 
-		 
+
 //		 String [] cardapio = new String [] {"Calabresa", "Atum", "Queijo", "Presunto"};
 //		 
 //		 cardapio[3] = "Frango"; //Alterando a posição 3
 //		 for (int i = 0; i < cardapio.length; i++) { // length - quantos itens o vetor possui
 //				System.out.println(cardapio[i]); //Vamos mostrar a opção alterada
 //			}
-		
-		String [] cardapio = new String [4];
-		
+
+		String[] cardapio = new String[4];
+
 		cardapio[0] = "Calabresa";
 		cardapio[1] = "Atum";
 		cardapio[2] = "Queijo";
 		cardapio[3] = "Presunto";
-		
+
 		for (int i = 0; i < cardapio.length; i++) {
 			System.out.println(cardapio[i]);
 		}
 
 	}
-
 }
