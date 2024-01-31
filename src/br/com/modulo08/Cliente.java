db623f828bc42368bd7e37a129d3ac62e1664030
silva3265 <silva3265@gmail.com>
2024-01-31 16:24:33 -0300

    Metodos de Instancia

---
+++ src/br/com/modulo08/Cliente.java
@@ -0,0 +1,25 @@
+package br.com.modulo08;
+
+public class Cliente {
+
+	String primeiroNome;
+
+	String ultimoNome;
+
+	String telefone;
+
+	String email;
+
+	String obterNomeCompleto() {
+		String nomeCompleto = primeiroNome + " " + ultimoNome;
+
+		return nomeCompleto;
+	}
+
+	String obterDDD() {
+
+		String ddd = telefone.substring(0, 2);
+		return ddd;
+	}
+
+}
