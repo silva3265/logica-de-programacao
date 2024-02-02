709ffdd5704ec8051f1f172473ac1b122f096141
silva3265 <silva3265@gmail.com>
2024-02-02 16:43:58 -0300

    Lendo informações de um arquivo de texto

---
+++ src/br/com/modulo10/LeituraArquivos.java
@@ -0,0 +1,23 @@
+package br.com.modulo10;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class LeituraArquivos {
+
+	public static void main(String[] args) throws IOException {
+		Path arquivo = Paths.get("/tmp/aula/arquivo.txt");
+
+		List<String> linhas = Files.readAllLines(arquivo);
+
+		for (int i = 0; i < linhas.size(); i++) {
+			String nome = linhas.get(i); //na Lista passamos o indice dentro do get
+
+			System.out.println("Jogador " + i + ": " + nome);
+		}
+	}
+
+}
